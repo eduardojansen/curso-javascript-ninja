c891f3cb8cdd3ea938e9b004c7219c4159ab6f7e
eduardojansen <eduardo.j@hostdime.com.br>
2019-12-03 13:36:17 -0300

    update

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -16,7 +16,7 @@ function addItem(item) {
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-console.log(addItem(['item01','item02','item03']));
+console.log(addItem([3,true,'item03']));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
